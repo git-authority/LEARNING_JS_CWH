45a7d06cfeba861d674f3a551122173b726b2fa7
Abhrasnata Ray <abhrasnataray@gmail.com>
2024-07-30 10:59:48 +0530

    Test

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function showConfirm() {
-    // Display a confirmation dialog
-    let userResponse = confirm("Do you want to proceed?");
-
-    // Check the user's response
-    if (userResponse) {
-        alert("You clicked OK!");
-    } else {
-        alert("You clicked Cancel!");
-    }
-}
+document.getElementById("fun").style.backgroundColor = "red";
